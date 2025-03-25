15495636f86a796b2ac876cb2651a0c3a2ea58c5
AndrewBezotchestvo <03051820dr@gmail.com>
2025-03-25 20:00:45 +0800

    add new elements

--- tap tap dash/Assets/scripts/PlayerMove.cs
+++ tap tap dash/Assets/scripts/PlayerMove.cs
@@ -5,6 +5,7 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float _playerSpeed;
     [SerializeField] private float _jumpForce;
+    [SerializeField] private float _gravityScale;
 
     private Rigidbody _rb;
 
@@ -12,22 +13,54 @@ public class PlayerMove : MonoBehaviour
 
     private bool _direction;
 
+    private bool _isGround;
+
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _direction = true;
+        _movement = transform.forward;
     }
 
     private void Update()
     {
-        _movement = transform.forward;
-        _rb.MovePosition(transform.position + _movement * _playerSpeed * Time.deltaTime);
-
         if (Input.GetMouseButtonDown(0))
         {
             _direction = !_direction;
 
             StartCoroutine(rotate(_direction));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGround)
+        {
+            _isGround = false;
+            _movement.y = _jumpForce;
+        }
+
+        if (_direction)
+        {
+            _movement.z = _playerSpeed;
+            _movement.x = 0;
+        }
+        else
+        {
+            _movement.x = -_playerSpeed;
+            _movement.z = 0;
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.MovePosition(transform.position + _movement * Time.fixedDeltaTime);
+
+        if (!_isGround)
+        {
+            _movement.y -= _gravityScale * Time.fixedDeltaTime;
+        }
+        else
+        {
+            _movement.y = 0;
         }
     }
 
@@ -44,7 +77,15 @@ public class PlayerMove : MonoBehaviour
                 transform.localRotation = Quaternion.Euler(transform.localRotation.x, -i, transform.localRotation.z);
             }
 
-            yield return new WaitForSeconds(0.002f);
+            yield return new WaitForSeconds(0.001f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            _isGround = true;
         }
     }
 }
